90280ba489c7e9a8d97776ef62605bda2e7d73b7
ZEAL <tassadiqshakeel5@gmail.com>
2023-01-08 14:36:01 +0500

    day_4

---
+++ 2022/day_4.py
@@ -0,0 +1,21 @@
+counter = 0
+def check(fa:int,sa:int):
+    global counter
+    a = set(range(int(fa[0]),int(fa[1])+1))
+    b = set(range(int(sa[0]),int(sa[1])+1))
+    if b.issubset(a) == True:
+        counter += 1
+        return
+    elif a.issubset(b) == True:
+        counter += 1
+        return
+
+
+with open("zzzinp.text",'r') as file:
+    file = file.readlines()
+    for i in file:
+        aa = i.strip().split(',')
+        ab = [k.split('-') for k in aa]
+        check(ab[0],ab[1])
+
+print(counter)
